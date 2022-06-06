e12d26c3c421dfc5ca496a49a683896d4ea7ef46
Valentina <v.vasileva@nuzhnapomosh.ru>
2022-06-06 17:35:02 +0300

    add brain even game

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,28 @@
 import readlineSync from 'readline-sync';
 
-const run = () => {
+const ROUNDS_COUNT = 3;
+
+const runEngine = (questionsAndAnswers, description) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  console.log(description);
+
+  for (let round = 1; round <= ROUNDS_COUNT; round += 1) {
+    const [question, rightAnswer] = questionsAndAnswers[round - 1];
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer !== rightAnswer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${name}!`);
 };
 
-export default run;
+const getRandomArbitrary = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+export { runEngine, getRandomArbitrary };
+export { ROUNDS_COUNT };
---
+++ src/games/even.js
@@ -0,0 +1,16 @@
+import { runEngine, getRandomArbitrary, ROUNDS_COUNT } from '../cli.js';
+
+const isEven = (number) => number % 2 === 0;
+
+export default () => {
+  const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const questionsAndAnswers = [];
+
+  for (let gameNumber = 1; gameNumber <= ROUNDS_COUNT; gameNumber += 1) {
+    const question = getRandomArbitrary(1, 1000);
+    const answer = isEven(question) ? 'yes' : 'no';
+    questionsAndAnswers.push([question, answer]);
+  }
+
+  runEngine(questionsAndAnswers, description);
+};
